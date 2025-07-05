0b102eb080cb52c3b868e6f9cd5292b614791293
steve xiaohu zhao <steve_zhao@163.com>
2025-07-05 16:45:37 +0800

    add invoke deploy module in test module

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -14,8 +14,16 @@
 
 module.exports = async ({ getNamedAccounts, deployment }) => {
     const firstAccount = (await getNamedAccounts()).firstAccount
-    console.log(`first account is ${firstAccount}`)
-    console.log("it is a deploy function.")
+    const { deploy } = deployments
+
+    await deploy("FundMe", {
+        from: firstAccount,
+        args: [180],
+        log: true
+    })
 }
 
+// npx hardhat deploy --tags all
+// npx hardhat deploy --tags funme
+module.exports.tags = ["all", "fundme"]
 
--- test/fundme.test.js
+++ test/fundme.test.js
@@ -3,25 +3,40 @@ const { assert } = require("chai")
 describe("test fundme contract", async function () {
     // 准备写单元测试
 
+    // 定义两个变量，随后在 beforeEach() 中赋值
+    // 在其他的 it() 函数中可以直接使用这两个变量
+    let fundMe
+    let firstAccount
+
+    // 在每个 it() 执行之前都会执行 beforeEach()
+    beforeEach(async function () {
+        // 在该测试脚本中，调用部署脚本。部署了所有 tag 为 all 的合约
+        await deployments.fixture("all")
+        firstAccount = (await getNamedAccounts()).firstAccount
+        const fundMeDeployment = await deployments.get("FundMe")
+        fundMe = await ethers.getContractAt("FundMe", fundMeDeployment.address)
+    })
+
+
     // 测试 FundMe.sol 中的构造函数中的 owner 是否为 msg.sender
     it("test if the owner is msg.sender", async function () {
         // 部署合约
-        const [firstAccount] = await ethers.getSigners()
-        const fundMeFactory = await ethers.getContractFactory("FundMe")
-        const fundMe = await fundMeFactory.deploy(180)
+        // const [firstAccount] = await ethers.getSigners()
+        // const fundMeFactory = await ethers.getContractFactory("FundMe")
+        // const fundMe = await fundMeFactory.deploy(180)
         await fundMe.waitForDeployment()
 
         // 执行测试
-        assert.equal((await fundMe.owner()), firstAccount.address)
+        assert.equal((await fundMe.owner()), firstAccount)  // firstAccount 本身就是地址了
     })
 
 
     // 测试 FundMe.sol 中的构造函数中的 dataFeed
     it("test if the dataFeed is assign correctly", async function () {
         // 部署合约
-        const [firstAccount] = await ethers.getSigners()
-        const fundMeFactory = await ethers.getContractFactory("FundMe")
-        const fundMe = await fundMeFactory.deploy(180)
+        // const [firstAccount] = await ethers.getSigners()
+        // const fundMeFactory = await ethers.getContractFactory("FundMe")
+        // const fundMe = await fundMeFactory.deploy(180)
         await fundMe.waitForDeployment()
 
         // 执行测试
